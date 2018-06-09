5464dbf8e5efea13d213899b202a858c5cba4e48
Stefan Fochler <istefo@me.com>
2018-06-10 00:44:48 +0200

    Add Xcode project

---
+++ CocoaHTTPServer/CocoaHTTPServer.h
@@ -0,0 +1,23 @@
+//
+//  CocoaHTTPServer.h
+//  CocoaHTTPServer
+//
+//  Copyright (c) 2011, Deusty, LLC
+//
+//  This is just robbiehanson/CocoaHTTPServer wrapped in a modern Xcode project that exposes a
+//  module called CocoaHTTPServer.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for CocoaHTTPServer.
+FOUNDATION_EXPORT double CocoaHTTPServerVersionNumber;
+
+//! Project version string for CocoaHTTPServer.
+FOUNDATION_EXPORT const unsigned char CocoaHTTPServerVersionString[];
+
+#import <CocoaHTTPServer/HTTPServer>
+#import <CocoaHTTPServer/HTTPConnection>
+#import <CocoaHTTPServer/HTTPMessage>
+#import <CocoaHTTPServer/HTTPAuthenticationRequest>
+#import <CocoaHTTPServer/HTTPResponse>
