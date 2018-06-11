7f218c9c6ebe2f3821f1de885867db82f28d489e
Stefan Fochler <istefo@me.com>
2018-06-11 08:52:58 +0200

    Expose more headers

--- CocoaHTTPServer/CocoaHTTPServer.h
+++ CocoaHTTPServer/CocoaHTTPServer.h
@@ -16,8 +16,18 @@ FOUNDATION_EXPORT double CocoaHTTPServerVersionNumber;
 //! Project version string for CocoaHTTPServer.
 FOUNDATION_EXPORT const unsigned char CocoaHTTPServerVersionString[];
 
-#import <CocoaHTTPServer/HTTPServer>
-#import <CocoaHTTPServer/HTTPConnection>
-#import <CocoaHTTPServer/HTTPMessage>
-#import <CocoaHTTPServer/HTTPAuthenticationRequest>
-#import <CocoaHTTPServer/HTTPResponse>
+#import <CocoaHTTPServer/HTTPServer.h>
+#import <CocoaHTTPServer/HTTPConnection.h>
+#import <CocoaHTTPServer/HTTPMessage.h>
+#import <CocoaHTTPServer/HTTPAuthenticationRequest.h>
+#import <CocoaHTTPServer/HTTPResponse.h>
+// Problematic because it would expose DDLog.h which should probably be another framework itself...
+// #import <CocoaHTTPServer/HTTPLogging.h>
+#import <CocoaHTTPServer/WebSocket.h>
+
+#import <CocoaHTTPServer/HTTPAsyncFileResponse.h>
+#import <CocoaHTTPServer/HTTPRedirectResponse.h>
+#import <CocoaHTTPServer/HTTPDataResponse.h>
+#import <CocoaHTTPServer/HTTPFileResponse.h>
+#import <CocoaHTTPServer/HTTPDynamicFileResponse.h>
+#import <CocoaHTTPServer/HTTPErrorResponse.h>
